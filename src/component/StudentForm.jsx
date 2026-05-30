dc4984c1b0a7830c2308fb62f0f3ed526d58aa97
Durabas Behera <durabasbehera@Durabass-MacBook-Air.local>
2026-05-30 12:33:19 +0530

    chore: added validation to the student form

--- src/component/StudentForm.jsx
+++ src/component/StudentForm.jsx
@@ -45,37 +45,37 @@ const StudentForm =()=>{
                                 <div className="mb-3">
                                     <label className="form-label">Name:</label>
                                     <br />
-                                    <input type="text" placeholder="Name" value={name} onChange={(e)=>setName(e.target.value)}/>
+                                    <input type="text" placeholder="Name" value={name} onChange={(e)=>setName(e.target.value)} required/>
                                 </div>
                                 <div className="mb-3">
                                     <label className="form-label">Age:</label>
                                     <br />
-                                    <input type="number" placeholder="Age" value={age} onChange={(e)=>setAge(e.target.value)}/>
+                                    <input type="number" placeholder="Age" value={age} onChange={(e)=>setAge(e.target.value)} required/>
                                 </div>
                                 <div className="mb-3">
                                     <label className="form-label">Grade:</label>
                                     <br />
-                                    <input type="text" placeholder="Grade" value={grade} onChange={(e)=>setGrade(e.target.value)}/>
+                                    <input type="text" placeholder="Grade" value={grade} onChange={(e)=>setGrade(e.target.value)} required/>
                                 </div>
                                 <div className="mb-3">
                                     <label className="form-label">Marks:</label>
                                     <br />
-                                    <input type="number" placeholder="Marks" value={marks} onChange={(e)=>setMarks(e.target.value)}/>
+                                    <input type="number" placeholder="Marks" value={marks} onChange={(e)=>setMarks(e.target.value)} required/>
                                 </div>
                                 <div className="mb-3">
                                     <label className="form-label">Attendance:</label>
                                     <br />
-                                    <input type="number" placeholder="Attendance" value={attendance} onChange={(e)=>setAttendance(e.target.value)}/>
+                                    <input type="number" placeholder="Attendance" value={attendance} onChange={(e)=>setAttendance(e.target.value)} required/>
                                 </div>
                                 <div className="mb-3">
                                     <label className="form-label d-block">Gender:</label>
                                     <div className="form-check form-check-inline">
-                                        <input className="form-check-input" type="radio" value="Male" checked={gender === "Male"} onChange={(e)=>setGender(e.target.value)}/>
-                                        <label className="form-check-lablel">Male</label>
+                                        <input className="form-check-input" type="radio" value="Male" checked={gender === "Male"} onChange={(e)=>setGender(e.target.value)} required/>
+                                        <label className="form-check-label">Male</label>
                                     </div>
                                     <div className="form-check form-check-inline">
-                                        <input className="form-check-input"  type="radio" value="Female" checked={gender === "Female"} onChange={(e)=>setGender(e.target.value)}/>
-                                        <label className="form-check-lablel">Female</label>
+                                        <input className="form-check-input"  type="radio" value="Female" checked={gender === "Female"} onChange={(e)=>setGender(e.target.value)} />
+                                        <label className="form-check-label">Female</label>
                                     </div>
                                 </div>
                                 <button type="submit" className="btn btn-primary">{student ? "Update Student" : "Add Student"}</button>
